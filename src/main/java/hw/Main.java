33b4e17d369306a3a84b992da03841932c80a343
aogle1 <aogle1@luc.edu>
2018-08-01 23:29:59 -0500

    Lab-10-ao-final

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -39,19 +39,20 @@ public class Main {
 
         System.out.println(theMap.toString());
 
-        System.out.println("\nBreadthfirst iteration starting from France");
+        System.out.println("\nBreadthfirst iteration starting from France:");
 
         final Iterator<String> bFirst = new BreadthFirstIterator<>(theMap, FR);
         while (bFirst.hasNext()) {
             final String country = bFirst.next();
             System.out.println(country);
         }
+        System.out.println("\nBreadthfirst iteration starting from Switzerland:");
         final Iterator<String> bFirst2 = new BreadthFirstIterator<>(theMap, CH);
         while (bFirst2.hasNext()) {
             final String country = bFirst2.next();
             System.out.println(country);
         }
-        System.out.println("\nClosestFirstIterator<> iteration starting from France");
+        System.out.println("\nClosestFirstIterator<> iteration starting from France:");
 
 
         final Iterator<String> cFirst = new ClosestFirstIterator<>(theMap, FR);
@@ -59,6 +60,8 @@ public class Main {
             final String country = cFirst.next();
             System.out.println(country);
         }
+        System.out.println("\nClosestFirstIterator<> iteration starting from Switzerland:");
+
 
 
         final Iterator<String> cFirst2 = new ClosestFirstIterator<>(theMap, CH);
@@ -66,16 +69,30 @@ public class Main {
             final String country = cFirst2.next();
             System.out.println(country);
         }
-        System.out.println("\nDepthFirstIterator<> iteration starting from France");
-        depthFirstIteratorTest(theMap, FR);
+        System.out.println("\nDepthFirstIterator<> iteration starting from France:");
+//        depthFirstIteratorTest(theMap, FR);
+        final Iterator<String> dFirst = new DepthFirstIterator<>(theMap, FR);
+        while (dFirst.hasNext()) {
+            final String country = dFirst.next();
+            System.out.println(country);
+        }
+        System.out.println("\nDepthFirstIterator<> iteration starting from Switzerland:");
 
-        System.out.println("\nRandomWalkIterator iteration starting from France");
+        final Iterator<String> dFirst2 = new DepthFirstIterator<>(theMap, CH);
+        while (dFirst2.hasNext()) {
+            final String country = dFirst2.next();
+            System.out.println(country);
+        }
+
+        System.out.println("\nRandomWalkIterator iteration starting from France:");
 
         final Iterator<String> rWalk = new RandomWalkIterator<>(theMap, FR, true, 6);
         while (rWalk.hasNext()) {
             final String country = rWalk.next();
             System.out.println(country);
         }
+
+        System.out.println("\nRandomWalkIterator iteration starting from Switzerland:");
         final Iterator<String> rWalk2 = new RandomWalkIterator<>(theMap, CH, true, 6);
         while (rWalk2.hasNext()) {
             final String country = rWalk2.next();
@@ -87,19 +104,19 @@ public class Main {
         System.out.println(mapColor.getColoring());
     }
 
-    public static void depthFirstIteratorTest(Graph<String, DefaultEdge> theMap, String countryCode){
-        final Iterator<String> dFirst = new DepthFirstIterator<>(theMap, countryCode);
-        while (dFirst.hasNext()) {
-            final String country = dFirst.next();
-            System.out.println(country);
-        }
+//    public static void depthFirstIteratorTest(Graph<String, DefaultEdge> theMap, String countryCode){ //Practice**
+//        final Iterator<String> dFirst = new DepthFirstIterator<>(theMap, countryCode);
+//        while (dFirst.hasNext()) {
+//            final String country = dFirst.next();
+//            System.out.println(country);
+//        }
 
 }
 
 
 
 
-}
+
 
 
 
